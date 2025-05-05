3c6890eae1df85bdbaea87689624012257c660e3
gdanca <gdanca@griddynamics.com>
2025-05-05 12:02:00 +0300

    Test 2

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  """Funcție care returnează 'Hello World!'."""
-  return 'Hello World!'
+  return 'Buna Sefilor!'
 
 
 if __name__ == '__main__':
