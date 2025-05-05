20b49118c527b757e7e3c11f23c61d323328d05d
gdanca <gdanca@griddynamics.com>
2025-05-05 13:50:58 +0300

    Fix error I think2

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,13 @@
 import pytest
 from app import app 
 
-@pytest.fixture
-def client():
-    app.config['TESTING'] = True
+# @pytest.fixture
+# def client():
+#     app.config['TESTING'] = True
 
-    with app.test_client() as client:
+#     with app.test_client() as client:
     
-        yield client
+#         yield client
 
 def test_status_code(client):
 
